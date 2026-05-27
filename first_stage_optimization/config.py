1410ba5941005966baa9687c4ffc289d961ddab8
kfourlaris <kfourlaris@ethz.ch>
2026-05-27 13:54:47 +0200

    Corrected the cooling COP

--- first_stage_optimization/config.py
+++ first_stage_optimization/config.py
@@ -4,8 +4,9 @@ import pandas as pd
 # --- GLOBAL SETTINGS (Subject to change) ---
 INTEREST_RATE = 0.12
 LIFESPAN = 20
-T_SINK = 65.0  # DH Supply Temperature
-T_RETURN = 25  # DH Return Temperature
+T_SINK = 70.0  # DH Supply Temperature
+T_RETURN = 30  # DH Return Temperature
+T_COOLING = 5  # DC Supply Temperature
 
 # --- ENERGY DEMAND ---
 # CHANGE THIS TO "Amsterdam" OR "Zurich" TO SWITCH THE ENTIRE MODEL
@@ -74,14 +75,14 @@ def _calculate_heating_cop(T_s_vec, T_k):
 
 def _calculate_cooling_cop(T_s_vec, T_k):
     dT = T_k - np.array(T_s_vec)
-    # The Regression Formula from (https://doi.org/10.1016/j.rser.2020.110646) for R717 refrigerant - 1
-    cop = (0.0014515 * (dT ** 2) - 0.23104 * dT + 11.684)-1
+    # The Regression Formula from (https://doi.org/10.1016/j.rser.2020.110646) for R717 refrigerant - 1) and reversible machines basic thermodynamics
+    cop = 0.0014515 * (dT ** 2) - 0.23104 * dT + 11.684 - 1
     return np.maximum(cop, 0.1).tolist()
 
 
 # --- EXPORTED VARIABLES ---
 ANNUITY_FACTOR = _calculate_annuity_factor(INTEREST_RATE, LIFESPAN)
 COP_VEC = _calculate_heating_cop(T_source_hourly, T_SINK)
-COP_COOL_VEC = _calculate_cooling_cop(T_source_hourly, T_SINK)
+COP_COOL_VEC = _calculate_cooling_cop(T_COOLING, T_source_hourly)
 
 
