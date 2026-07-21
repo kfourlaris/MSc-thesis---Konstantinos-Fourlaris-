3fdef625c8542f1d8dc4e7333db82d7de199956a
kfourlaris <kfourlaris@ethz.ch>
2026-07-21 21:01:42 +0300

    A tighter gap added to the sequential optimization approach

--- second_stage_optimization_new_approach/main3.py
+++ second_stage_optimization_new_approach/main3.py
@@ -23,7 +23,7 @@ print(f"Average cooling COP: {sum(config3.COP_COOL_VEC_15MIN) / 35040:.2f}")
 
 # --- STEP 1: INITIALIZE GUROBI MULTI-SCENARIO ENVIRONMENT ---
 model = gp.Model("Stage2_15Min_Balancing_Optimization_new_approach")
-model.setParam('MIPGap', 0.06)  # Maintain identical performance gap target
+model.setParam('MIPGap', 0.01)  # Maintain identical performance gap target
 # Force Gurobi to use the deterministic Barrier Method (skips concurrent wait)
 model.setParam('Method', 3)
 
